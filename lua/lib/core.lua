5be50a2753455e36d592dce4f512521d827ea3e8
dneumann <dneumann42@proton.me>
2024-10-08 19:31:31 -0700

    added download command

--- lua/lib/core.lua
+++ lua/lib/core.lua
@@ -8,4 +8,19 @@ function M.split_lines(input)
   return lines
 end
 
+local function get_download_command(out)
+  if vim.fn.executable('curl') == 1 then
+    return "curl '{}' -o " .. out
+  end
+  if vim.fn.executable('wget') == 1 then
+    return "wget '{}' -o " .. out
+  end
+  error("Thememanager requires 'curl' or 'wget'")
+end
+
+function M.download(url, out)
+  local front = get_download_command(out)
+  vim.cmd("!" .. front:gsub("{}", url))
+end
+
 return M
